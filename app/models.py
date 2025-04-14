5269b2754a0178321ef5912779712bcbfefbca20
brianp17 <bpostzakelijk@gmail.com>
2025-04-14 14:37:22 +0200

    ke

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from datetime import datetime
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -14,3 +15,9 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.name}>"
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column()
