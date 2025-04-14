e7405c5ace498e2cd70f54b0f964b9a6c0b4a308
brianp17 <bpostzakelijk@gmail.com>
2025-04-14 14:04:34 +0200

    typo

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    phonenumber = db.Column(db.String(20), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
