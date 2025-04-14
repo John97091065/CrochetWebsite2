9d7ef6d10df0c663f0efe0f5920a80983a307523
John97091065 <97091065@st.deltion.nl>
2025-04-14 14:01:18 +0200

    register

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, redirect, url_for, flash
+from werkzeug.security import generate_password_hash
+from .forms import RegisterForm
+from .models import db, User
 
 main = Blueprint('main', __name__)
 
@@ -12,4 +15,24 @@ def debug():
 
 @main.route('/register')
 def register():
-    return render_template('register.html')
+    form = RegisterForm()
+    if form.validate_on_submit():
+        existing_user = User.query.filter_by(email=form.email.data).first()
+        if existing_user:
+            flash('Email already registered. Try logging in.', 'warning')
+            return redirect(url_for('main.register'))
+
+        hashed_password = generate_password_hash(form.password.data)
+
+        new_user = User(
+            name=form.name.data,
+            email=form.email.data,
+            phonenumber=form.phone_number.data,
+            password_hashed=hashed_password
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Registration successful! You can now log in.', 'success')
+        return redirect(url_for('main.index'))
+
+    return render_template('register.html', form=form)
